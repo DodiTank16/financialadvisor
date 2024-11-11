4b5764b648d6fe615a52e539c53f89832f6600bc
utsav <utsavkachchhi500.com>
2024-11-11 23:52:14 +0530

    project setup

--- server.js
+++ server.js
@@ -86,13 +86,13 @@ app.use('/api/faq', faqRoutes);
 app.use('/api/terms', termsConditionsRoutes);
 app.use('/api/privacy', privacyPolicyRoutes);
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
-}
+// }
 const port = process.env.PORT || 6000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
